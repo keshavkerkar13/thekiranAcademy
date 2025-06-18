6480e578372045c34e48138d9818b14602716cd9
Keshav Kerkar <keshavkerkar19@gmail.com>
2025-06-18 19:12:06 +0530

    Add files via upload

---
+++ MultifunCalculator/src/TestMulCalci.java
@@ -0,0 +1,13 @@
+
+public class TestMulCalci {
+
+	public static void main(String[] args) {
+		MultifunCalculator c1 = new MultifunCalculator();
+		System.out.println("Start of main method");
+		c1.addTwo(20, 25);
+		c1.subTwo(20, 25);
+		c1.mulTwo(20, 25);
+		c1.divTwo(20, 25);
+		System.out.println("End of main method");
+	}
+}
