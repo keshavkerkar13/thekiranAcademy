cd37fe28bc674492abba1529eb53878d655e0d9c
Keshav Kerkar <keshavkerkar19@gmail.com>
2025-06-18 19:09:55 +0530

    Add files via upload

---
+++ MyFan/src/TestFan.java
@@ -0,0 +1,10 @@
+
+public class TestFan {
+
+	public static void main(String[]args) {
+		MyFan f1 = new MyFan();
+		System.out.println("Start of main method");
+		f1.addSpeed(4,5);
+		System.out.println("End of the main method");
+	}
+}
