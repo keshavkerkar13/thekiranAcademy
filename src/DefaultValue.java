5f49d7f50e8dd3aafe3b16b5416d5582793fccc8
Keshav Kerkar <keshavkerkar19@gmail.com>
2025-07-04 09:02:58 +0530

    Add files via upload

---
+++ src/DefaultValue.java
@@ -0,0 +1,12 @@
+
+public class DefaultValue {
+	public int a;
+	public double d;
+	public float f;
+	public byte b;
+	public short sh;
+	public long l;
+	public char ch;
+	public boolean bl;
+	public String s;
+}
---
+++ src/TestDef.java
@@ -0,0 +1,16 @@
+
+public class TestDef {
+	public static void main(String[] args) {
+		DefaultValue dv=new DefaultValue();
+		System.out.println(dv.a);
+		System.out.println(dv.d);
+		System.out.println(dv.f);
+		System.out.println(dv.b);
+		System.out.println(dv.sh);
+		System.out.println(dv.l);
+		System.out.println(dv.ch);
+		System.out.println(dv.bl);
+		System.out.println(dv.s);	
+	}
+
+}
